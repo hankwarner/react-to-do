a2691cf327146cbb3e7019cf3874886be762c63d
hankwarner <40283259+hankwarner@users.noreply.github.com>
2018-07-25 20:23:44 -0400

    updated assignemnt code for Slack help

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 class ToDo extends Component {
+    
     render() {
         return (
             <li>
                 <input type="checkbox" checked={ this.props.isCompleted } onChange={ this.props.toggleComplete } />
                 <span> { this.props.description } </span>
-                <button deleteTodo={this.props.deleteTodo} onClick={this.deleteTodo.bind(item)}>Delete</button> 
+                <button onClick={() => this.props.deleteTodo(this)}>Delete</button> 
             </li>
         );
     }
